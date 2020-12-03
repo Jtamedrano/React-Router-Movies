dcbf3c75aeae744148f2d908a5252574cf48fc99
Jesse Medrano <Jtamedrano@gmail.com>
2020-12-02 18:50:15 -0800

    mvp met

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
   const params = useParams();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
-  console.log(params);
+
   useEffect(() => {
     axios
       .get(`http://localhost:5000/api/movies/${params.id}`) // Study this endpoint with Postman
@@ -20,8 +21,6 @@ export default function Movie(props) {
       .catch((error) => {
         console.error(error);
       });
-    // This effect should run every time time
-    // the `id` changes... How could we do this?
   }, []);
 
   // Uncomment this only when you have moved on to the stretch goals
@@ -31,27 +30,5 @@ export default function Movie(props) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map((star) => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+  return <MovieCard movie={movie} />;
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 export default function MovieList(props) {
   return (
@@ -12,19 +13,9 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
-  const { id, title, director, metascore } = props.movie;
-
   return (
-    <Link to={`/movie/${id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-      </div>
+    <Link to={`/movie/${props.movie.id}`}>
+      <MovieCard movie={props.movie} />
     </Link>
   );
 }
